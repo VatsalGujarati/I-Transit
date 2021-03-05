6d563c035e327dc8e37cbda73ebac8b04ceefd41
VatsalGujarati <vatsal1568@gmail.com>
2021-03-05 16:16:20 +0530

    css changed

--- js/monitor.js
+++ js/monitor.js
@@ -1,66 +1,51 @@
 //geting detail for count
-  // Initialize Firebase        
+// Initialize Firebase
 var db = firebase.firestore();
 
-function getcount()
-{
-    var date = document.getElementById("select_date").value;
-    var arr = date.split('-');
-    var date_chane = arr.concat(arr[1] + '-' + arr[2] + '-' + arr[0]);
-    var date_scan = date_chane[3];
-    var bus_no = document.getElementById("bus_no").value;
-    console.log(date_scan);
-    var count=0;
-  //window.alert(document.getElementById("select_date").innerHTML = date);
-   //window.alert(document.getElementById("bus_no").innerHTML = bus_no);
-      // var docRef = db.collection("CAMERA_COUNT").doc(date);
-      //var docRef = db.collection(bus_no);
-       
-      var docRef = db.collection("CAMERA_COUNT").doc(date).collection(bus_no);
-      docRef.get().then(querySnapshot => {
+function getcount() {
+  var date = document.getElementById("select_date").value;
+  var arr = date.split("-");
+  var date_chane = arr.concat(arr[1] + "-" + arr[2] + "-" + arr[0]);
+  var date_scan = date_chane[3];
+  var bus_no = document.getElementById("bus_no").value;
+  console.log(date_scan);
+  var count = 0;
+  // var peopleCount = document.getElementById("peaple_count").innerHTML;
+  // var ticketCount = document.getElementById("ticket_count").innerHTML;
 
-        var length = querySnapshot.size;
-        var docData = [];
-        
-        querySnapshot.forEach(doc => {
-            //console.log(doc.id, " => ", doc.data());
-            docData.push(doc.data());
-            })
-            
-        //console.log(docData);
-        var last = docData[docData.length-1];
-        console.log(last);
-        console.log(last.current);
+  ////////////////// camera count ///////////////////////////
+  var docRef = db.collection("CAMERA_COUNT").doc(date).collection(bus_no);
+  docRef
+    .get()
+    .then((querySnapshot) => {
+      var length = querySnapshot.size;
+      var docData = [];
 
-        document.getElementById("peaple_count").innerHTML = last.in; 
-        
-        }).catch((error) => {
-           console.log("Error getting document:", error);
-       });  
- 
+      querySnapshot.forEach((doc) => {
+        docData.push(doc.data());
+      });
 
-      // var docRef1 = db.collection("ONLINE_TICKET_SCANNING").doc(date_scan).collection(bus_no);
-       //var docRef2 = docRef1.collection(bus_no);
-       //console.log(docRef1);
+      var last = docData[docData.length - 1];
+      console.log(last);
+      console.log(last.current);
 
+      document.getElementById("peaple_count").innerHTML = last.in;
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
 
-    //   docRef1.array.forEach(element => {
-    //       count = count + 1;    
-    //   });
+  ////////////////////// ticket scannig ///////////////////////
+  db.collection("ONLINE_TICKET_SCANNING")
+    .doc(date_scan)
+    .collection(bus_no)
+    .get()
+    .then((querySnapshot) => {
+      console.log(querySnapshot.size);
 
-       
-    db.collection("ONLINE_TICKET_SCANNING").doc(date_scan).collection(bus_no).get().then(querySnapshot => {
-        console.log(querySnapshot.size);
-        // querySnapshot.forEach(doc => {
-        //   //  console.log(doc.id, " => ", doc.data());
-        //     //orderid = doc.id.orderBy("time", "desc").limit(3);
-        //     //console.log(orderid);
-        //     })
-        document.getElementById("ticket_count").innerHTML = querySnapshot.size; 
-
-        }).catch((error) => {
-           console.log("Error getting document:", error);
-       });  
-
-
-} 
+      document.getElementById("ticket_count").innerHTML = querySnapshot.size;
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
+}
